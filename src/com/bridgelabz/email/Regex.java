ce0a6e1365900d7c530a8f029f4e28c51885aa4e
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-28 12:39:23 +0530

    [Prerna]ADDED: Mandatory starting string

--- src/com/bridgelabz/email/Regex.java
+++ src/com/bridgelabz/email/Regex.java
@@ -1,9 +1,28 @@
 package com.bridgelabz.email;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
 public class Regex {
+	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-z]+$");
+	private static Scanner sc;
+    
+	private boolean checkEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
 	
 	public static void main(String args[]) {
 		System.out.println("Welcome to Email Validation!");
+		Regex r = new Regex();
+		sc = new Scanner(System.in);
+		System.out.println("Enter Email: ");
+		String email = sc.nextLine();
+		boolean emailValid = r.checkEmail(email);
+		if(emailValid)
+			System.out.println("Email is VALID!");
+		else
+			System.out.println("ERROR! Email is INVALID!");
 	}
 
 }
